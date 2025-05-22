6f8838ae7b20f0dbd3b44138dee81005b76371c7
Ridwan Malik <34468231+ridwanmalik@users.noreply.github.com>
2025-05-22 13:53:36 +0600

    feat(transactions): consolidate transaction columns into LastStatement and TransactionList components for improved readability and maintainability

--- app/(merchant)/dashboard/_components/LastStatement.tsx
+++ app/(merchant)/dashboard/_components/LastStatement.tsx
@@ -1,8 +1,58 @@
 "use client"
 import { LoadingTable } from "@/components/ui"
 import { useMerchantTransactionsQuery } from "@/store"
+import { DataTableColumn } from "@/types"
+import { formatPrice } from "@/utils"
+import moment from "moment"
 import DataTable from "react-data-table-component"
-import { columns } from "../transactions/_components/column"
+
+const columns: DataTableColumn = [
+  {
+    name: "SL",
+    selector: (row: any, index?: number) => (index !== undefined ? index + 1 : ""),
+    width: "55px",
+  },
+  {
+    name: "Order ID",
+    selector: (row: any) => row.merchant_tran_id,
+    minWidth: "135px;",
+  },
+  {
+    name: "Creation date",
+    selector: (row: any) => moment(row.gw_txn_timestamp).format("lll"),
+    minWidth: "160px",
+  },
+  {
+    name: "Order Amount",
+    selector: (row: any) => formatPrice(row.merchant_order_amount),
+  },
+  {
+    name: "Refund Amount",
+    selector: (row: any) => formatPrice(row.refund_amount ? row.refund_amount : 0),
+  },
+  {
+    name: "MSF Fee",
+    selector: (row: any) => formatPrice(row.pgw_charge + row.bank_charge),
+  },
+  {
+    name: "Payment Mode",
+    selector: (row: any) => row.paymode || "--",
+  },
+  {
+    name: "Settlement Amount",
+    selector: (row: any) => formatPrice(row.settlement_amount),
+  },
+
+  {
+    name: "Settlement Status",
+    selector: (row: any) => "--",
+  },
+
+  {
+    name: "Order Status",
+    selector: (row: any) => row.status,
+  },
+]
 
 export const LastStatement = () => {
   const { data, isLoading } = useMerchantTransactionsQuery({
@@ -14,7 +64,7 @@ export const LastStatement = () => {
       <DataTable
         className=""
         title="Recent Transactions"
-        columns={columns.splice(0, 7)}
+        columns={columns}
         data={(data?.data || [])?.slice(0, 10)}
         pagination={false}
         progressPending={isLoading}
